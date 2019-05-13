692384aaa8383b865c271c97fe075507310b9d53
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-05-13 14:20:22 -0400

    Updates

---
+++ precache-manifest.293326ef692b7bb6ea62fcc725760d8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6380f54b735d82bee20",
+    "url": "/static/css/main.01766adc.chunk.css"
+  },
+  {
+    "revision": "c6380f54b735d82bee20",
+    "url": "/static/js/main.c6380f54.chunk.js"
+  },
+  {
+    "revision": "b04fcf0433455235d0d0",
+    "url": "/static/js/1.b04fcf04.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18d40436920f72cc796deb56629e1e6f",
+    "url": "/static/media/dark-green-glow-29241-1920x1080(1).18d40436.jpg"
+  },
+  {
+    "revision": "4b9b28e58fb8609d87156c77c3e4ae5d",
+    "url": "/static/media/yo.4b9b28e5.png"
+  },
+  {
+    "revision": "923826950e4c5145693abf32dfdd3514",
+    "url": "/static/media/Cv.- AnaPaulaMonsalveTapia(1).92382695.pdf"
+  },
+  {
+    "revision": "801613392ba6481536b591f22f06c971",
+    "url": "/index.html"
+  }
+];
